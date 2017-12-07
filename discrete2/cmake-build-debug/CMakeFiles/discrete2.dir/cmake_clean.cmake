158f088a3fd83e25324066a7c671e7aa0889995a
nazmicancalik <nazmicancalik@gmail.com>
2017-12-07 09:21:43 +0300

    Discrete 2 is added. Start of project 2.

---
+++ discrete2/cmake-build-debug/CMakeFiles/discrete2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/discrete2.dir/main.cpp.o"
+  "CMakeFiles/discrete2.dir/PCB.cpp.o"
+  "CMakeFiles/discrete2.dir/SchedulingManager.cpp.o"
+  "discrete2.pdb"
+  "discrete2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/discrete2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
